ae38a356c99905fd6d532d5549a3f4c3834c50d4
Henrietta <henrietta.rye@duke.edu>
2023-09-18 02:31:57 -0400

    Added a banner for a movie on top on page

--- src/Banner.js
+++ src/Banner.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from './axios';
+import requests from './requests';
 
 function Banner() {
+    const [movie, setMovie]=useState([]);
+
+    useEffect(()=>{
+        async function fetchData (){
+            const request= await axios.get(requests.fetchMovieOriginals);
+            setMovie(request.data.results[Math.floor(Math.random()* request.data.results.length)]);
+            return request;
+
+        }
+        fetchData();
+    },[]);
+
+
   return (
-    <header>
+    <header className="banner"
+    style={{
+        backgroundSize:"cover",
+        backgroundImage:`url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition:"center center"
+    }}
+    
+    >
+        
+        <div className='banner_contents'></div>
+        <h1>
+            {movie?.title || movie?.name||movie?.original_name}
+             </h1>
       
     </header>
   )
 }
 
-export default Banner
+export default Banner;
