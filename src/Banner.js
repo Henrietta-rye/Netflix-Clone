14882a24fca87bd829aa746c2a2ef5cfd50180a7
Henrietta <henrietta.rye@duke.edu>
2023-09-21 20:51:41 -0400

    Added Buttons and Description to banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import axios from './axios';
 import requests from './requests';
+import './Banner.css';
+
 
 function Banner() {
     const [movie, setMovie]=useState([]);
@@ -15,6 +17,17 @@ function Banner() {
         fetchData();
     },[]);
 
+    function truncate(str, n){
+        return str?.length>n?str.substr(0,n-1)+"...":str;
+     
+     
+     
+     
+       }
+     
+
+   
+
 
   return (
     <header className="banner"
@@ -27,9 +40,18 @@ function Banner() {
     >
         
         <div className='banner_contents'></div>
-        <h1>
+        <h1 className='banner_title'> 
             {movie?.title || movie?.name||movie?.original_name}
-             </h1>
+            </h1>
+            <div className='banner_buttons'>
+                <button className='banner_button'>Play</button>
+                <button className='banner_button'>My List</button>
+                <h1 className='banner_description'>
+                    {truncate(movie?.overview,150)}
+                </h1>
+                
+            </div>
+
       
     </header>
   )
